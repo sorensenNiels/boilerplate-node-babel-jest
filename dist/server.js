2a7039dbe603fbafead9e61351e24e7e2555a4f7
sorensenNiels <sorensen.niels@gmail.com>
2018-11-27 21:11:11 +0100

    Added cross-env

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,7 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/* eslint-disable no-console */
 var app = (0, _express.default)();
 var port = 3000;
 app.get('/', function (req, res) {
